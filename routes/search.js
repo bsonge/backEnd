91305d897d5831425b86fc484bba031c38b4584c
Quintin Donnelly <quinndonnelly22@gmail.com>
2018-04-26 08:51:15 -0500

    Csv download (#49)
    * Added the file download option for advanced search
    
    - if the query has file parameter set to true then a file will be sent
    - files are housed in temp until they are finished downloading then
      they are deleted
    
    * Added the basic search putting each model csv into zip
    
    - the basic search will return a zip file with csvs for each model
    - it currently doens't handle if one of the models has no elements
      this will be fixed next update
    - I MAKE NO CLAIM TO ELEGANCE SIMPLY FUNCTIONALITY SORRY!! (>'')>
    
    * This fixes breaking error if the result is empty
    
    - Logs the error to out which on server will be the log file

--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const jsonResponse = require('../utils/response');
 const Helper = require('../helpers/helper');
+const fs = require('fs');
+const path = require('path');
+const shortid = require('shortid');
+const zip = new require('node-zip')();
 
 // Limit default on queries
 const DEFAULT_RETURN_LIMIT = 25;
@@ -23,6 +27,61 @@ const TARGET_FIELDS = [
   'technological_target_official_symbol',
   'technological_target_gene_symbol',
 ];
+
+/**
+ * Constructs a file into the temp directory this file will have a csv format
+ * with the contents of obj in side of it the fields will be the keys
+ * @param  {Object} obj  This is the object that holds the data to be put in the
+ *                       file
+ * @param  {STRING} name This is the name of the file (be sure it's unique)
+ * @return {Promise}      Will resolve when done (doesn't reject currently)
+ */
+buildFile = (obj, name) => {
+  return new Promise(async (resolve, reject) => {
+    const output = fs.createWriteStream(`temp/${name}`, {encoding: 'utf8'});
+    const keys = Object.keys(obj[0].dataValues);
+    for (let i = 0; i < keys.length; i += 1) {
+      if (i+1 === keys.length) {
+        output.write(`${keys[i]}\n`);
+      } else {
+        output.write(`${keys[i]}, `);
+      }
+    }
+
+    let needQuote = false;
+    for (let i = 0; i < obj.length; i += 1) {
+      for (let j = 0; j < keys.length; j += 1) {
+        needQuote = false;
+        if (typeof(obj[i].dataValues[keys[j]]) === 'string') {
+          obj[i].dataValues[keys[j]] = obj[i].dataValues[keys[j]]
+                                      .replace(/\r?\n|\r/g, '\\n');
+          if (obj[i].dataValues[keys[j]].search(',') !== -1) {
+            output.write('\"');
+            needQuote = true;
+          }
+        }
+
+        if (j+1 === keys.length) {
+          output.write(`${obj[i].dataValues[keys[j]]}\n`);
+        } else {
+          output.write(`${obj[i].dataValues[keys[j]]}`);
+        }
+
+        if (needQuote) {
+          output.write('\"');
+        }
+
+        if (j+1 !== keys.length) {
+          output.write(',');
+        }
+      }
+    }
+    output.end();
+    output.on('close', () => {
+      resolve();
+    });
+  });
+};
 
 router.route('/basic')
   .get(async (req, res) => {
@@ -71,6 +130,72 @@ router.route('/basic')
       chemical: data[0],
       target: data[1],
     };
+
+    if (req.query.file) {
+      const targetFileName = `target-${shortid.generate()}.csv`;
+      const chemicalFileName = `chemical-${shortid.generate()}.csv`;
+
+      const fileConstruction = [];
+      if (result.chemical.length !== 0) {
+        fileConstruction.push(buildFile(result['chemical'], chemicalFileName));
+      }
+      if (result.target.length !== 0) {
+        fileConstruction.push(buildFile(result['target'], targetFileName));
+      }
+
+      await Promise.all(fileConstruction);
+
+      if (result.target.length !== 0) {
+        zip.file('target.csv', fs.readFileSync(
+          path.join(__dirname, '../temp', targetFileName)
+        ));
+      }
+      if (result.chemical.length !== 0) {
+        zip.file('chemical.csv', fs.readFileSync(
+          path.join(__dirname, '../temp', chemicalFileName)
+        ));
+      }
+
+      let data = zip.generate({base64: false, compression: 'DEFLATE'});
+
+      const zipFileName = `zip-${shortid.generate()}.zip`;
+      fs.writeFileSync(
+        path.join(__dirname, '../temp', zipFileName),
+        data,
+        'binary'
+      );
+      res.download(
+        path.join(__dirname, '../temp', zipFileName),
+        `results.zip`,
+        (err) => {
+          if (err) console.log(err);
+          fs.unlink(path.join(__dirname, '../temp', zipFileName), (err) => {
+            console.log(
+              'Error in basic query file zip delete',
+              err
+            );
+          });
+          fs.unlink(path.join(__dirname, '../temp', chemicalFileName),
+            (err) => {
+              console.log(
+                'Error in basic query file chemical delete',
+                err
+              );
+            }
+          );
+          fs.unlink(path.join(__dirname, '../temp', targetFileName), (err) => {
+            if (err) {
+              console.log(
+                'Error in basic query file target delete',
+                err
+              );
+            }
+          });
+        }
+      );
+
+      return;
+    }
 
     res.status(200).json(new jsonResponse(null, result));
   });
@@ -134,6 +259,35 @@ router.route('/advanced/:model')
       res.status(500).josn(new jsonResponse(err));
       return;
     }
+
+    // If the user specified they want a file send results in file
+    if (req.query.file && results.length === 0) {
+      res.status(404).json(new jsonResponse('Empty query no matches...'));
+      return;
+    }
+
+    if (req.query.file && results.length !== 0) {
+      const fileName = `${modelName}-${shortid.generate()}.csv`;
+      await buildFile(results, fileName);
+      res.download(
+        path.join(__dirname, '../temp', fileName),
+        `${modelName}.csv`,
+        (err) => {
+          console.log(
+            'Error in advanced query download csv',
+            err
+          );
+          fs.unlink(path.join(__dirname, '../temp', fileName), (err) => {
+            console.log(
+              'Error in advanced query file delete',
+              err
+            );
+          });
+        });
+      return;
+    }
+
     res.status(200).json(new jsonResponse(null, results));
   });
+
 module.exports = router;
